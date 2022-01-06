473131de8a9eefc5795d8a49e594d8c90cc7d66a
unknown <danilo.bonaminjr@gmail.com>
2022-01-06 10:44:08 -0300

    segundo commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Clientes-Front'`, () => {
+  it(`should have as title 'Clientes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Clientes-Front');
+    expect(app.title).toEqual('Clientes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Clientes-Front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Clientes app is running!');
   });
 });
